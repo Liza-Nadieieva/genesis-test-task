cb2cb4d7eb4cd2874708ef134f388acee07d8f7c
Liza-Nadieieva <56040231+Liza-Nadieieva@users.noreply.github.com>
2020-10-29 18:49:55 +0200

    add header with styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Header } from 'components';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <div className="container-wrapper">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+export const Header = () => {
+  return (
+    <div className={classes.header}>
+      <h2 className={classes.header__title}>Test Task</h2>
+      <div className={classes.logo_wrapper}>
+        <div className={classes.logo_star}></div>
+        <h2 className={classes.header__title}>Genesis</h2>
+      </div>
+
+      <div className={classes.star_1}></div>
+      <div className={classes.star_2}></div>
+      <div className={classes.star_3}></div>
+    </div>
+  )
+}
